8cfcb2cf71b7099ee4c4df5266d074b1c5ac8292
NelsonWang <wangnelson2@gmail.com>
2022-05-04 15:08:15 -0400

    Production update |

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,11 +26,11 @@ function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
+      <div className="conta piner">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          {getLink('java', '2b')}
+          {getLink('python', '2a')}
         </div>
       </div>
     </header>
