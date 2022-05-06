59318c907e52ee54372761c3891c748bcea63a64
NelsonWang <wangnelson2@gmail.com>
2022-05-06 09:15:45 -0400

    container

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,7 +26,7 @@ function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="conta piner">
+      <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
