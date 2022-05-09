0601a9d5eb0965badbb6d42c35b51008827ff0f2
NelsonWang <wangnelson2@gmail.com>
2022-05-09 03:49:39 -0400

    project 2a | custom components

---
+++ src/components/test-admonition.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Admonition from '@theme/Admonition';
+
+
+export default function AdmonitionTest() {
+  return (
+    <div>
+      <Admonition type="info">
+        <p>Some information</p>
+      </Admonition>
+    </div>
+  );
+}
