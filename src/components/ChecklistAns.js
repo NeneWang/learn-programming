1542ff07edf3947263f5fb0555d1b315565618a0
NelsonWang <wangnelson2@gmail.com>
2022-05-23 11:23:25 -0400

    checklist

--- src/components/ChecklistAns.js
+++ src/components/ChecklistAns.js
@@ -3,8 +3,10 @@ import React, { Children } from 'react';
 import CheckBox from "@site/src/components/CheckBox.js";
 
 
-export default function CheckListAns({ checkList }) {
+export default function CheckList({ checkList }) {
 
+  checkList = ["Test1", "Tes2"]
+  answer = 1
   let List = checkList.map(check => <CheckBox title={check} />)
 
   return (
