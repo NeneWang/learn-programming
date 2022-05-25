71439c448c7cd1bfc2bb956aab721ae255f9d683
NelsonWang <wangnelson2@gmail.com>
2022-05-25 00:16:41 -0400

    user response

--- src/components/ChecklistAns.js
+++ src/components/ChecklistAns.js
@@ -8,11 +8,11 @@ export default function CheckListAns({ checkList }) {
   checkList = ["Test1", "Tes2"]
   const answer = 1
   let checkBoxList = checkList.map(check => <CheckBox title={check} />)
-  const  [userResponse, setUserResponse] = useState(checkList.map(checkItem => {checkItem: false}));
+  const [userResponse, setUserResponse] = useState(checkList.map(checkItem => { checkItem: false }));
 
   const toggleAnswer = (answer) => {
     // What you want to do here is to change. The adaaptability with 
-  
+
     // If on the list then mark it as not on the list. 
     setUserResponse(!userResponse[answer])
   }
@@ -20,9 +20,9 @@ export default function CheckListAns({ checkList }) {
 
 
   return (
-    <div> 
-      {checkBoxList}
-      
+    <div>
+      {userResponse[checkList[0]] ? checkBoxList : ""}
+
     </div>
   );
 }
