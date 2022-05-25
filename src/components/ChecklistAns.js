1dc6a73e33c2c21ad11940ccf100bac450177de8
NelsonWang <wangnelson2@gmail.com>
2022-05-25 00:15:29 -0400

    checklist | ans

--- src/components/ChecklistAns.js
+++ src/components/ChecklistAns.js
@@ -1,5 +1,5 @@
 
-import React, { Children } from 'react';
+import React, { useState } from 'react';
 import CheckBox from "@site/src/components/CheckBox.js";
 
 
@@ -8,14 +8,13 @@ export default function CheckListAns({ checkList }) {
   checkList = ["Test1", "Tes2"]
   const answer = 1
   let checkBoxList = checkList.map(check => <CheckBox title={check} />)
-  const  [userResponse, setUserResponse] = useState([]);
+  const  [userResponse, setUserResponse] = useState(checkList.map(checkItem => {checkItem: false}));
 
-  const toggleAnswer = (answers) => {
+  const toggleAnswer = (answer) => {
     // What you want to do here is to change. The adaaptability with 
-    
-
+  
     // If on the list then mark it as not on the list. 
-
+    setUserResponse(!userResponse[answer])
   }
 
 
@@ -23,6 +22,7 @@ export default function CheckListAns({ checkList }) {
   return (
     <div> 
       {checkBoxList}
+      
     </div>
   );
 }
