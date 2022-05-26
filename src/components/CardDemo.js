bf90aef5736ed0e05a2f25e3e154c28241461f09
NelsonWang <wangnelson2@gmail.com>
2022-05-26 19:39:45 -0400

    component notes | cards

--- src/components/CardDemo.js
+++ src/components/CardDemo.js
@@ -1,10 +1,35 @@
 import React from "react"
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
-export default function CardDemo(){
+export default function CardDemo() {
 
-return (
-    <></>
-)
+    return (
+        <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                    Word of the Day
+                </Typography>
+                <Typography variant="h5" component="div">asdas
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                    adjective
+                </Typography>
+                <Typography variant="body2">
+                    well meaning and kindly.
+                    <br />
+                    {'"a benevolent smile"'}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">Learn More</Button>
+            </CardActions>
+        </Card>
+    )
 
 }
 
