3538fb702604bbb0866b8d14277ec765f8192f9f
NelsonWang <wangnelson2@gmail.com>
2022-05-25 21:09:17 -0400

    sliders | img

---
+++ src/components/Slider.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+
+export default function Slider({ }) {
+
+    return (
+        <img src={require('../../static/img/2022-05-09-03-30-09.png').default} width="100%" />
+    )
+
+}
+
+
