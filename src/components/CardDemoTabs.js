337ae8b0091ac62c5c38ec9e02314943c1dbe263
NelsonWang <wangnelson2@gmail.com>
2022-05-26 20:42:09 -0400

    carddemo | tabs

--- src/components/CardDemoTabs.js
+++ src/components/CardDemoTabs.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
@@ -7,6 +7,11 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
 
 import Chip from '@mui/material/Chip';
 import Stack from '@mui/material/Stack';
@@ -17,9 +22,32 @@ const image2 = require("../../static/img/2022-05-26-20-01-45.png").default
 
 export default function CardDemo({ title = "", tags = ["Machine Learning", "Programming"], links = linkSample }) {
 
+    const [value, setValue] = React.useState(1);
+
+    // const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    //     setValue(newValue);
+    // };
+    const handleChange = (newValue) => {
+        setValue(newValue);
+    };
+
     return (
         <Card sx={{ minWidth: 275 }}>
-            <CardContent>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                <TabContext value={value}>
+                    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                        <TabList onChange={handleChange} aria-label="lab API tabs example">
+                            <Tab label="Item One" value="1" />
+                            <Tab label="Item Two" value="2" />
+                            <Tab label="Item Three" value="3" />
+                        </TabList>
+                    </Box>
+                    <TabPanel value="1">Item One</TabPanel>
+                    <TabPanel value="2">Item Two</TabPanel>
+                    <TabPanel value="3">Item Three</TabPanel>
+                </TabContext>
+            </Box>
+            {/* <CardContent>
 
                 <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
                     <Stack direction="row" spacing={1}>
@@ -52,7 +80,7 @@ export default function CardDemo({ title = "", tags = ["Machine Learning", "Prog
                         malesuada lacus ex, sit amet blandit leo lobortis eget.
                     </Typography>
                 </AccordionDetails>
-            </Accordion>
+            </Accordion> */}
         </Card>
     )
 
