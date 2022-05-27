431f95b0c994e19963b0f6a976453decfa723732
NelsonWang <wangnelson2@gmail.com>
2022-05-26 20:16:36 -0400

    card demo

--- src/components/CardDemo.js
+++ src/components/CardDemo.js
@@ -5,10 +5,15 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
 
 
 const linkSample = { "Udemy": "https://www.udemy.com/course/tensorflow-developer-certificate-machine-learning-zero-to-mastery/", "My Notes": "https://docs.google.com/document/d/1kP4wv0uMWKvrFWUlq66kVZnvnyb0km1AmkP65-dFJqQ/edit" }
-
+const image = require("@site/static/img/2022-05-26-19-56-57.png").default
+const image2 = require("../../static/img/2022-05-26-20-01-45.png").default
 
 export default function CardDemo({ title = "", tags = ["Machine Learning", "Programming"], links = linkSample }) {
 
@@ -25,14 +30,29 @@ export default function CardDemo({ title = "", tags = ["Machine Learning", "Prog
                         return <span><a href={links[linkItem]} target="_blank" >{linkItem}</a>  | </span>
                     })}
                 </Typography>
+                <img src={image2} />
                 <Typography variant="body2">
                     well meaning and kindly.
                     <br />
                     {'"a benevolent smile"'}
                 </Typography>
             </CardContent>
-            <CardActions>
-            </CardActions>
+
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                >
+                    <Typography>Accordion 1</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                        malesuada lacus ex, sit amet blandit leo lobortis eget.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
         </Card>
     )
 
